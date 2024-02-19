f920d0cc7cf66044a5af2aa7b46420d167873284
Nandhakumar S <kumarnandha8983@gmail.com>
2024-02-19 22:49:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="flex flex-col w-[100vwh] h-[100vwh] items-center justify-center bg-gray-400">
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <div className="bg-gray-500 text-white flex items-center justify-center">
+            <p className="text-2xl font-bold">Header</p>
+        </div>
+    );
+}
+
+export default Header;
